b43949287096425a8710c266516545b1736c96e5
hardik.joshi <hardik.joshi@eclinicalworks.com>
2020-03-02 03:26:42 -0500

    Step:2 - Implementing the Comparator.comparing Static Method and Making the Key Extractor Generic, Returning Comparable Objects

--- src/com/pluralsight/java/lambdaexpression/comparatorexample/MainComparator.java
+++ src/com/pluralsight/java/lambdaexpression/comparatorexample/MainComparator.java
@@ -12,5 +12,11 @@ public class MainComparator {
         Function<Person, Integer> f1 = p -> p.getAge();
         Function<Person, String> f2 = p -> p.getLastName();
         Function<Person, String> f3 = p -> p.getFirstName();
+
+        // We can use f1 in the parameter or we can also simplify p -> p.getAge() we can even simplify by method
+        // reference Person::getAge
+        Comparator<Person> cmpPersonAge = Comparator.comparing(Person::getAge);
+        Comparator<Person> cmpPersonLastName = Comparator.comparing(Person::getLastName);
+        Comparator<Person> cmpPersonFirstName = Comparator.comparing(Person::getFirstName);
     }
 }
