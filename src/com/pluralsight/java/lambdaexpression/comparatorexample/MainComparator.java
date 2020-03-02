6f8388333fec85408d0a35b06df372b789096293
hardik.joshi <hardik.joshi@eclinicalworks.com>
2020-03-02 03:08:23 -0500

    Step:1 - Refactoring the Comparison with a Key Extractor

--- src/com/pluralsight/java/lambdaexpression/comparatorexample/MainComparator.java
+++ src/com/pluralsight/java/lambdaexpression/comparatorexample/MainComparator.java
@@ -1,11 +1,16 @@
 package com.pluralsight.java.lambdaexpression.comparatorexample;
 
-import java.util.Arrays;
+import java.util.function.Function;
 
 public class MainComparator {
     public static void main(String[] args) {
         Comparator<Person> cmpAge = (p1, p2) -> p2.getAge() - p1.getAge();
         Comparator<Person> cmpLastName = (p1, p2) -> p1.getLastName().compareTo(p2.getLastName());
         Comparator<Person> cmpFirstName = (p1, p2) -> p1.getFirstName().compareTo(p2.getFirstName());
+
+        // Instead of writing above code we can write Function which will take Person and return Integer or String
+        Function<Person, Integer> f1 = p -> p.getAge();
+        Function<Person, String> f2 = p -> p.getLastName();
+        Function<Person, String> f3 = p -> p.getFirstName();
     }
 }
