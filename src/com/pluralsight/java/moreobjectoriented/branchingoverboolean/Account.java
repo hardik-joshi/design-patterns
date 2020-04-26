f456c6ee17545961440082bd1e7bc677e2a0c92f
Hardik Joshi <joshi.hardik654@gmail.com>
2020-04-26 21:29:28 +0530

    Creating basic Account class to deposit and withdraw money

---
+++ src/com/pluralsight/java/moreobjectoriented/branchingoverboolean/Account.java
@@ -0,0 +1,13 @@
+package com.pluralsight.java.moreobjectoriented.branchingoverboolean;
+
+import java.math.BigDecimal;
+
+public class Account {
+    public void deposit(BigDecimal amount) {
+
+    }
+
+    public void withdraw(BigDecimal amount) {
+
+    }
+}
