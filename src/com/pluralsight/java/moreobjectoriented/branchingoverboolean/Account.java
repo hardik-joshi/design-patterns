9944a286f366d60d7a30b4789d518b1b7b25daa4
Hardik Joshi <joshi.hardik654@gmail.com>
2020-04-26 22:23:42 +0530

    New requirement: client can close the account

--- src/com/pluralsight/java/moreobjectoriented/branchingoverboolean/Account.java
+++ src/com/pluralsight/java/moreobjectoriented/branchingoverboolean/Account.java
@@ -4,18 +4,32 @@ import java.math.BigDecimal;
 
 public class Account {
     private boolean isVerified;
+    private boolean isClosed;
+    private BigDecimal balance;
+
+    public Account() {
+        balance = BigDecimal.ZERO;
+    }
 
     public void holderVerified() {
         isVerified = true;
     }
 
-    public void deposit(BigDecimal amount) {
+    public void closeAccount() {
+        isClosed = true;
+    }
 
+    public void deposit(BigDecimal amount) {
+        if (!isClosed)
+            return; // Or do something more meaningful
+        this.balance = this.balance.add(amount);
     }
 
     public void withdraw(BigDecimal amount) {
-        if(!isVerified)
+        if (!isVerified)
             return; // Or do something more meaningful
-        // Withdraw money
+        if (!isClosed)
+            return;
+        this.balance = this.balance.subtract(amount);
     }
 }
