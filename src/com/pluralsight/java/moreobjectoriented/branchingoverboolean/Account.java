5d925170c1c468f9cfc6269c6155f40a7ab39f74
Hardik Joshi <joshi.hardik654@gmail.com>
2020-04-26 22:11:53 +0530

    client have requirement: account should be verified

--- src/com/pluralsight/java/moreobjectoriented/branchingoverboolean/Account.java
+++ src/com/pluralsight/java/moreobjectoriented/branchingoverboolean/Account.java
@@ -3,11 +3,19 @@ package com.pluralsight.java.moreobjectoriented.branchingoverboolean;
 import java.math.BigDecimal;
 
 public class Account {
+    private boolean isVerified;
+
+    public void holderVerified() {
+        isVerified = true;
+    }
+
     public void deposit(BigDecimal amount) {
 
     }
 
     public void withdraw(BigDecimal amount) {
-
+        if(!isVerified)
+            return; // Or do something more meaningful
+        // Withdraw money
     }
 }
