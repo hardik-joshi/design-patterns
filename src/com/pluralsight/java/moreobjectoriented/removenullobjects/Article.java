33cfd7d20a0c7e72a3e54bcb016186559ad31059
Hardik Joshi <joshi.hardik654@gmail.com>
2020-05-06 23:35:58 +0530

    Turning an Object into a Finite State Machine

--- src/com/pluralsight/java/moreobjectoriented/removenullobjects/Article.java
+++ src/com/pluralsight/java/moreobjectoriented/removenullobjects/Article.java
@@ -3,13 +3,19 @@ package com.pluralsight.java.moreobjectoriented.removenullobjects;
 public class Article {
     private Warranty moneyBackGuarantee;
     private Warranty expressWarranty;
+    private Warranty effectiveExpressWarranty;
 
     public Article(Warranty moneyBackGuarantee, Warranty expressWarranty) {
+        this(moneyBackGuarantee, expressWarranty, Warranty.VOID);
+    }
+
+    private Article(Warranty moneyBackGuarantee, Warranty expressWarranty, Warranty effectiveExpressWarranty) {
         if(moneyBackGuarantee == null) throw new IllegalArgumentException();
         if (expressWarranty == null) throw new IllegalArgumentException();
 
         this.moneyBackGuarantee = moneyBackGuarantee;
         this.expressWarranty = expressWarranty;
+        this.effectiveExpressWarranty = effectiveExpressWarranty;
     }
 
     public Warranty getMoneyBackGuarantee() {
@@ -17,6 +23,14 @@ public class Article {
     }
 
     public Warranty getExpressWarranty() {
-        return expressWarranty;
+        return effectiveExpressWarranty;
+    }
+
+    public Article withVisibleDamage() {
+        return new Article(Warranty.VOID, this.expressWarranty, this.effectiveExpressWarranty);
+    }
+
+    public Article notOperational() {
+        return new Article(this.moneyBackGuarantee, this.expressWarranty, this.expressWarranty);
     }
 }
--- src/com/pluralsight/java/moreobjectoriented/removenullobjects/Demo.java
+++ src/com/pluralsight/java/moreobjectoriented/removenullobjects/Demo.java
@@ -24,16 +24,20 @@ public class Demo {
 
     private void run() {
         LocalDate sellingDate = LocalDate.now().minus(40, ChronoUnit.DAYS);
-        Warranty moneyBack1 = new TimeLimitedWarranty(sellingDate, Duration.ofDays(30));
+        Warranty moneyBack1 = new TimeLimitedWarranty(sellingDate, Duration.ofDays(60));
         Warranty warranty1 = new TimeLimitedWarranty(sellingDate, Duration.ofDays(365));
 
         Article item1 = new Article(moneyBack1, warranty1);
 
         this.claimWarranty(item1);
+        this.claimWarranty(item1.withVisibleDamage());
+        this.claimWarranty(item1.notOperational().withVisibleDamage());
+        this.claimWarranty(item1.notOperational());
 
         Article item2 = new Article(Warranty.VOID, Warranty.lifetime(sellingDate));
 
         this.claimWarranty(item2);
+        this.claimWarranty(item2.notOperational().withVisibleDamage());
     }
 
     public static void main(String[] args) {
