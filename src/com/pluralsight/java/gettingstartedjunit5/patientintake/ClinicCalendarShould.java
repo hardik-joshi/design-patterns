8db20bd040b41af7e4967de8edb6c859f939b005
Hardik Joshi <joshi.hardik654@gmail.com>
2020-05-10 23:49:00 +0530

    Applying some more Assertion tests

--- src/com/pluralsight/java/gettingstartedjunit5/patientintake/ClinicCalendarShould.java
+++ src/com/pluralsight/java/gettingstartedjunit5/patientintake/ClinicCalendarShould.java
@@ -2,6 +2,7 @@ package com.pluralsight.java.gettingstartedjunit5.patientintake;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
@@ -10,8 +11,8 @@ import static org.junit.jupiter.api.Assertions.*;
 class ClinicCalendarShould {
 
     @Test
-    public void allowEntryOfAnAppointment() {
-        ClinicCalendar clinicCalendar = new ClinicCalendar();
+    void allowEntryOfAnAppointment() {
+        ClinicCalendar clinicCalendar = new ClinicCalendar(LocalDate.now());
         clinicCalendar.addAppointment("Jim", "Weaver", "avery",
                 "09/01/2020 02:00 pm");
         List<PatientAppointment> appointments = clinicCalendar.getAppointments();
@@ -20,8 +21,32 @@ class ClinicCalendarShould {
         PatientAppointment enteredAppt = appointments.get(0);
         assertEquals("Jim", enteredAppt.getPatientFirstName());
         assertEquals("Weaver", enteredAppt.getPatientLastName());
-        assertEquals(Doctor.avery, enteredAppt.getDoctor());
+        assertSame(Doctor.avery, enteredAppt.getDoctor());
         assertEquals("9/1/2020 02:00 PM",
                 enteredAppt.getAppointmentDateTime().format(DateTimeFormatter.ofPattern("M/d/yyyy hh:mm a")));
+    }
+
+    @Test
+    void returnTrueForHasAppointmentsIfThereAreAppointments() {
+        ClinicCalendar clinicCalendar = new ClinicCalendar(LocalDate.now());
+        clinicCalendar.addAppointment("Jim", "Weaver", "avery",
+                "09/01/2020 02:00 pm");
+        assertTrue(clinicCalendar.hasAppointment(LocalDate.of(2020, 9, 1)));
+    }
+
+    @Test
+    void returnFalseForHasAppointmentsIfThereAreNoAppointments() {
+        ClinicCalendar clinicCalendar = new ClinicCalendar(LocalDate.now());
+        assertFalse(clinicCalendar.hasAppointment(LocalDate.of(2020, 9, 1)));
+    }
+
+    @Test
+    void returnCurrentDaysAppointment() {
+        ClinicCalendar calendar = new ClinicCalendar(LocalDate.now());
+        calendar.addAppointment("Hardik", "Joshi", "avery",
+                "05/10/2020 11:00 AM");
+        calendar.addAppointment("Bhavesh", "Patel", "avery",
+                "05/10/2020 11:30 AM");
+        assertEquals(2, calendar.getTodayAppointments().size());
     }
 }
--- src/com/pluralsight/java/gettingstartedjunit5/patientintake/ClinicMain.java
+++ src/com/pluralsight/java/gettingstartedjunit5/patientintake/ClinicMain.java
@@ -1,6 +1,8 @@
 package com.pluralsight.java.gettingstartedjunit5.patientintake;
 
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Scanner;
 
 public class ClinicMain {
@@ -8,7 +10,7 @@ public class ClinicMain {
     private static ClinicCalendar calendar;
 
     public static void main(String[] args) throws Throwable {
-        calendar = new ClinicCalendar();
+        calendar = new ClinicCalendar(LocalDate.now());
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the Patient Intake Computer System!\n\n");
         String lastOption = "";
@@ -22,6 +24,7 @@ public class ClinicMain {
         System.out.println("Please select an option:");
         System.out.println("1. Enter a Patient Appointment");
         System.out.println("2. View All Appointments");
+        System.out.println("3. View Today's Appointments");
         System.out.println("X.  Exit System.");
         System.out.print("Option: ");
         String option = scanner.next();
@@ -31,6 +34,9 @@ public class ClinicMain {
                 return option;
             case "2":
                 performAllAppointments();
+                return option;
+            case "3":
+                performTodayAppointments();
                 return option;
             default:
                 System.out.println("Invalid option, please re-enter.");
@@ -60,15 +66,27 @@ public class ClinicMain {
 
     private static void performAllAppointments() throws Throwable {
         System.out.println("\n\nAll Appointments in System:");
-        for (PatientAppointment appointment : calendar.getAppointments()) {
+        listAppointments(calendar.getAppointments());
+        System.out.println("\nPress any key to continue...");
+        System.in.read();
+        System.out.println("\n\n");
+    }
+
+    private static void performTodayAppointments() throws Throwable {
+        System.out.println("\n\nAppointments for Today:");
+        listAppointments(calendar.getTodayAppointments());
+        System.out.println("\nPress any key to continue...");
+        System.in.read();
+        System.out.println("\n\n");
+    }
+
+    private static void listAppointments(List<PatientAppointment> appointments) {
+        for (PatientAppointment appointment : appointments) {
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("M/d/yyyy hh:mm a");
             String apptTime = formatter.format(appointment.getAppointmentDateTime());
             System.out.println(String.format("%s:  %s, %s\t\tDoctor: %s", apptTime, appointment.getPatientLastName(),
                     appointment.getPatientFirstName(), appointment.getDoctor().getName()));
         }
-        System.out.println("\nPress any key to continue...");
-        System.in.read();
-        System.out.println("\n\n");
     }
 
 }
