050d1962ac88a0f909c6edd9ba301797618988af
Hardik Joshi <joshi.hardik654@gmail.com>
2020-05-11 22:03:28 +0530

    Setting up and Tearing Down Tests

--- src/com/pluralsight/java/gettingstartedjunit5/patientintake/ClinicCalendarShould.java
+++ src/com/pluralsight/java/gettingstartedjunit5/patientintake/ClinicCalendarShould.java
@@ -1,6 +1,6 @@
 package com.pluralsight.java.gettingstartedjunit5.patientintake;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -9,13 +9,25 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ClinicCalendarShould {
+    private ClinicCalendar calendar;
+
+    @BeforeAll
+    static void testClassSetup() {
+        System.out.println("Before all...");
+    }
+
+    @BeforeEach
+    void init() {
+        System.out.println("Before each...");
+        calendar = new ClinicCalendar(LocalDate.of(2020, 9, 1));
+    }
 
     @Test
     void allowEntryOfAnAppointment() {
-        ClinicCalendar clinicCalendar = new ClinicCalendar(LocalDate.now());
-        clinicCalendar.addAppointment("Jim", "Weaver", "avery",
+        System.out.println("entry of appointment...");
+        calendar.addAppointment("Jim", "Weaver", "avery",
                 "09/01/2020 02:00 pm");
-        List<PatientAppointment> appointments = clinicCalendar.getAppointments();
+        List<PatientAppointment> appointments = calendar.getAppointments();
         assertNotNull(appointments);
         assertEquals(1, appointments.size());
         PatientAppointment enteredAppt = appointments.get(0);
@@ -28,25 +40,35 @@ class ClinicCalendarShould {
 
     @Test
     void returnTrueForHasAppointmentsIfThereAreAppointments() {
-        ClinicCalendar clinicCalendar = new ClinicCalendar(LocalDate.now());
-        clinicCalendar.addAppointment("Jim", "Weaver", "avery",
+        System.out.println("has appts...");
+        calendar.addAppointment("Jim", "Weaver", "avery",
                 "09/01/2020 02:00 pm");
-        assertTrue(clinicCalendar.hasAppointment(LocalDate.of(2020, 9, 1)));
+        assertTrue(calendar.hasAppointment(LocalDate.of(2020, 9, 1)));
     }
 
     @Test
     void returnFalseForHasAppointmentsIfThereAreNoAppointments() {
-        ClinicCalendar clinicCalendar = new ClinicCalendar(LocalDate.now());
-        assertFalse(clinicCalendar.hasAppointment(LocalDate.of(2020, 9, 1)));
+        System.out.println("no appts...");
+        assertFalse(calendar.hasAppointment(LocalDate.of(2020, 9, 1)));
     }
 
     @Test
     void returnCurrentDaysAppointment() {
-        ClinicCalendar calendar = new ClinicCalendar(LocalDate.now());
+        System.out.println("current days appts...");
         calendar.addAppointment("Hardik", "Joshi", "avery",
-                "05/10/2020 11:00 AM");
+                "09/01/2020 11:00 AM");
         calendar.addAppointment("Bhavesh", "Patel", "avery",
-                "05/10/2020 11:30 AM");
+                "09/01/2020 11:30 AM");
         assertEquals(2, calendar.getTodayAppointments().size());
+    }
+
+    @AfterEach
+    void tearDownEachTest() {
+        System.out.println("After each...");
+    }
+
+    @AfterAll
+    static void tearDownTestClass() {
+        System.out.println("After all...");
     }
 }
