bfdb438ba6e18a59c7131b28cb2abd779485cc97
Hardik Joshi <joshi.hardik654@gmail.com>
2020-05-12 22:07:35 +0530

    remove unnecessary code in unit test

--- src/com/pluralsight/java/gettingstartedjunit5/patientintake/ClinicCalendarShould.java
+++ src/com/pluralsight/java/gettingstartedjunit5/patientintake/ClinicCalendarShould.java
@@ -11,20 +11,13 @@ import static org.junit.jupiter.api.Assertions.*;
 class ClinicCalendarShould {
     private ClinicCalendar calendar;
 
-    @BeforeAll
-    static void testClassSetup() {
-        System.out.println("Before all...");
-    }
-
     @BeforeEach
     void init() {
-        System.out.println("Before each...");
         calendar = new ClinicCalendar(LocalDate.of(2020, 9, 1));
     }
 
     @Test
     void allowEntryOfAnAppointment() {
-        System.out.println("entry of appointment...");
         calendar.addAppointment("Jim", "Weaver", "avery",
                 "09/01/2020 02:00 pm");
         List<PatientAppointment> appointments = calendar.getAppointments();
@@ -33,8 +26,8 @@ class ClinicCalendarShould {
         PatientAppointment enteredAppt = appointments.get(0);
 
         assertAll(
-            () -> assertEquals("Jims", enteredAppt.getPatientFirstName()),
-            () -> assertEquals("Weavers", enteredAppt.getPatientLastName()),
+            () -> assertEquals("Jim", enteredAppt.getPatientFirstName()),
+            () -> assertEquals("Weaver", enteredAppt.getPatientLastName()),
             () -> assertSame(Doctor.avery, enteredAppt.getDoctor()),
             () -> assertEquals("9/1/2020 02:00 PM",
                     enteredAppt.getAppointmentDateTime().format(DateTimeFormatter.ofPattern("M/d/yyyy hh:mm a")))
@@ -43,7 +36,6 @@ class ClinicCalendarShould {
 
     @Test
     void returnTrueForHasAppointmentsIfThereAreAppointments() {
-        System.out.println("has appts...");
         calendar.addAppointment("Jim", "Weaver", "avery",
                 "09/01/2020 02:00 pm");
         assertTrue(calendar.hasAppointment(LocalDate.of(2020, 9, 1)));
@@ -51,28 +43,15 @@ class ClinicCalendarShould {
 
     @Test
     void returnFalseForHasAppointmentsIfThereAreNoAppointments() {
-        System.out.println("no appts...");
         assertFalse(calendar.hasAppointment(LocalDate.of(2020, 9, 1)));
     }
 
     @Test
-    @Disabled
     void returnCurrentDaysAppointment() {
-        System.out.println("current days appts...");
         calendar.addAppointment("Hardik", "Joshi", "avery",
                 "09/01/2020 11:00 AM");
         calendar.addAppointment("Bhavesh", "Patel", "avery",
                 "09/01/2020 11:30 AM");
         assertEquals(2, calendar.getTodayAppointments().size());
-    }
-
-    @AfterEach
-    void tearDownEachTest() {
-        System.out.println("After each...");
-    }
-
-    @AfterAll
-    static void tearDownTestClass() {
-        System.out.println("After all...");
     }
 }
