f2c23f71effe37f9884e2ce72bf3024e68bdb37c
Hardik Joshi <joshi.hardik654@gmail.com>
2020-05-22 21:29:13 +0530

    Checking in Which Thread a Task is Running

--- src/com/pluralsight/java/asychronousprogrammingusingcompletestage/settingupasyncprogramming/FirstCompletableFutures.java
+++ src/com/pluralsight/java/asychronousprogrammingusingcompletestage/settingupasyncprogramming/FirstCompletableFutures.java
@@ -1,11 +1,17 @@
 package com.pluralsight.java.asychronousprogrammingusingcompletestage.settingupasyncprogramming;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class FirstCompletableFutures {
     public static void main(String[] args) throws InterruptedException {
-        CompletableFuture.runAsync(() -> System.out.println("I am running asynchronously in the thread "
-                + Thread.currentThread().getName()));
-        Thread.sleep(100);
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Runnable task = () -> {
+            System.out.println("I am running asynchronously in the thread " + Thread.currentThread().getName());
+        };
+        CompletableFuture.runAsync(task, executor);
+//        Thread.sleep(100);
+        executor.shutdown();
     }
 }
