f21b2f6b1879c4dde99d565b01f7c22301e864dd
Hardik Joshi <joshi.hardik654@gmail.com>
2020-05-22 21:20:54 +0530

    Which thread is the task running ?

--- src/com/pluralsight/java/asychronousprogrammingusingcompletestage/settingupasyncprogramming/FirstCompletableFutures.java
+++ src/com/pluralsight/java/asychronousprogrammingusingcompletestage/settingupasyncprogramming/FirstCompletableFutures.java
@@ -4,7 +4,8 @@ import java.util.concurrent.CompletableFuture;
 
 public class FirstCompletableFutures {
     public static void main(String[] args) throws InterruptedException {
-        CompletableFuture.runAsync(() -> System.out.println("I am running asynchronously"));
+        CompletableFuture.runAsync(() -> System.out.println("I am running asynchronously in the thread "
+                + Thread.currentThread().getName()));
         Thread.sleep(100);
     }
 }
