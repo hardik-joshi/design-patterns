df08cb25b7a231ce78cff00a245a4b497b41097e
Hardik Joshi <joshi.hardik654@gmail.com>
2020-05-24 00:03:45 +0530

    Delaying the Execution of a Supplier Asynchronously

--- src/com/pluralsight/java/asychronousprogrammingusingcompletestage/settingupasyncprogramming/CompletableFutureWithSupplier.java
+++ src/com/pluralsight/java/asychronousprogrammingusingcompletestage/settingupasyncprogramming/CompletableFutureWithSupplier.java
@@ -1,13 +1,23 @@
 package com.pluralsight.java.asychronousprogrammingusingcompletestage.settingupasyncprogramming;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.function.Supplier;
 
 public class CompletableFutureWithSupplier {
     public static void main(String[] args) {
-        Supplier<String> supplier = () -> Thread.currentThread().getName();
-        CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(supplier);
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Supplier<String> supplier = () -> {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+            }
+            return Thread.currentThread().getName();
+        };
+        CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(supplier, executor);
         String string = completableFuture.join();
         System.out.println("Result: " + string);
+        executor.shutdown();
     }
 }
