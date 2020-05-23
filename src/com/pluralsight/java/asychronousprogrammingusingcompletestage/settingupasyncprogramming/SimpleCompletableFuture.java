27bf2fafe620e23bd051e4b89406fa94cfe4ea2f
Hardik Joshi <joshi.hardik654@gmail.com>
2020-05-24 00:35:53 +0530

    Creating and Completing a Void CompletableFuture

---
+++ src/com/pluralsight/java/asychronousprogrammingusingcompletestage/settingupasyncprogramming/SimpleCompletableFuture.java
@@ -0,0 +1,23 @@
+package com.pluralsight.java.asychronousprogrammingusingcompletestage.settingupasyncprogramming;
+
+import java.util.concurrent.CompletableFuture;
+
+public class SimpleCompletableFuture {
+    public static void main(String[] args) {
+        CompletableFuture<Void> completableFuture = new CompletableFuture<>();
+
+        Runnable task = () -> {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+            }
+            completableFuture.complete(null);
+        };
+
+        CompletableFuture.runAsync(task);
+
+        Void nil = completableFuture.join();
+
+        System.out.println("we are done");
+    }
+}
